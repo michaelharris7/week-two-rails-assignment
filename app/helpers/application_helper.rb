2d067a6de8b07f7e7707d7eb92e192e49aa78182
Michael Harris <michaeljh7@gmail.com>
2018-05-30 15:20:40 -0600

    Added view helper for authentication links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+    if !current_admin
+      (link_to "Register", new_admin_registration_path) +
+      " ".html_safe +
+      (link_to "Sign In", new_admin_session_path)
+    else
+      link_to "Logout", destroy_admin_session_path, method: :delete
+    end
+  end
 end
